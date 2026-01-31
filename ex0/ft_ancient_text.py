851d8303ee4709a3002ff54e363c63af5f93d09d
Benjamin Beaurain <bbeaurai@c1r1p2.42lehavre.fr>
2026-01-31 12:04:04 +0100

    MAJ 31/01/26

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -1,6 +1,32 @@
 #! /bin/python3.10
 
-# verifier si le cofre existe avant de vouloir y entrer
+# =============================================================================
+# ================================= TEXT ======================================
+# =============================================================================
+
+
+def ft_ancient_text() -> None:
+    file = "ancient_fragment.txt"
+
+    print("Accessing Storage Vault: ", file)
+    try:
+        f = open(file, "r")
+    except FileNotFoundError:
+        print("\n" + "🚨​ ERROR: Storage vault not found. Run data generator "
+              "first.")
+    else:
+        print("Connection established..." + "\n")
+        print("RECOVERED DATA:")
+        print(f.read())
+        print("\n" + "Data recovery complete. Storage unit disconnected.")
+        f.close()
+
+
+# =============================================================================
+# ================================= MAIN ======================================
+# =============================================================================
+
 
 if __name__ == "__main__":
-    print(" CYBER ARCHIVES - DATA RECOVERY SYSTEM ".center(79, "="))
+    print(" CYBER ARCHIVES - DATA RECOVERY SYSTEM ".center(79, "=") + "\n")
+    ft_ancient_text()
