20b95658cbfe52bd46c6982e4f12c3e08f3639e2
Benjamin Beaurain <bbeaurai@c1r1p2.42lehavre.fr>
2026-01-31 12:03:38 +0100

    MAJ 31/01/26

---
+++ ex1/ft_archive_creation.py
@@ -0,0 +1,35 @@
+#! /bin/python3.10
+
+# =============================================================================
+# ================================= TEXT ======================================
+# =============================================================================
+
+def ft_archive_creation() -> None:
+    file = "new_discovery.txt"
+
+    text = [
+        "[ENTRY 001] New quantum algorithm discovered",
+        "[ENTRY 002] Efficiency increased by 347%",
+        "[ENTRY 003] Archived by Data Archivist trainee"
+            ]
+
+    print("Initializing new storage unit: ", file)
+    f = open(file, "w")
+    print("Storage unit created successfully..."+ "\n")
+
+    print("Inscribing preservation data...")
+    for line in text:
+        f.write(line + "\n")
+        print(line)
+    f.close()
+    print("\n" + "Data inscription complete. Storage unit sealed.")
+    print(f"Archive '{file}' ready for long-term preservation.")
+
+# =============================================================================
+# ================================= MAIN ======================================
+# =============================================================================
+
+
+if __name__ == "__main__":
+    print(" CYBER ARCHIVES - PRESERVATION SYSTEM ".center(79, "=") + "\n")
+    ft_archive_creation()
